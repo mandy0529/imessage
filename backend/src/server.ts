f4543188fcbd8fef46c6be1770f154e87abe67e5
minji <321963741@naver.com>
2023-03-09 15:27:25 +0900

    apollo setting ing

---
+++ backend/src/server.ts
@@ -0,0 +1,47 @@
+import { makeExecutableSchema } from "@graphql-tools/schema";
+import { ApolloServer } from "apollo-server-express";
+import {
+    ApolloServerPluginDrainHttpServer,
+    ApolloServerPluginLandingPageLocalDefault
+} from "apollo-server-core";
+
+import express from "express";
+import http from "http";
+import typeDefs from "./graphql/typeDefs";
+import resolvers from "./graphql/resolvers";
+
+async function startApolloServer() {
+    const app = express();
+    const httpServer = http.createServer(app);
+
+    const schema = makeExecutableSchema({
+        typeDefs,
+        resolvers
+    });
+
+    const server = new ApolloServer({
+        schema,
+        csrfPrevention: true,
+        cache: "bounded",
+        plugins: [
+            ApolloServerPluginDrainHttpServer({ httpServer }),
+            ApolloServerPluginLandingPageLocalDefault({ embed: true })
+        ]
+    });
+
+    await server.start();
+    server.applyMiddleware({
+        app,
+        path: "/"
+    });
+
+    await new Promise<void>((resolve) =>
+        httpServer.listen({ port: 4000 }, resolve)
+    );
+
+    console.log(
+        `🚀 Server ready at http://localhost:4000${server.graphqlPath}`
+    );
+}
+
+startApolloServer().catch((error) => console.log(error));
