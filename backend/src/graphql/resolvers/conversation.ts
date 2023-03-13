63ab3e1d1a8cf44d1c7c97121d9303f4bd8d81e3
minji <321963741@naver.com>
2023-03-13 17:04:52 +0900

    createConversation resolver

--- backend/src/graphql/resolvers/conversation.ts
+++ backend/src/graphql/resolvers/conversation.ts
@@ -1,15 +1,72 @@
+import { Prisma } from "@prisma/client";
+import { ApolloError } from "apollo-server-core";
 import { GraphQlContext } from "./../../utils/types";
 
 const resolvers = {
     Mutation: {
         createConversation: async (
             _: any,
-            args: { paticipantId: Array<string> },
+            args: { participantId: Array<string> },
             context: GraphQlContext
         ) => {
-            console.log("INSEDE CREATE CONVERSATION", args);
+            const { participantId } = args;
+            const { session, prisma } = context;
+
+            //  user가 없을때 에러
+            if (!session?.user) {
+                throw new ApolloError("Not authorized");
+            }
+
+            const {
+                user: { id: userId }
+            } = session;
+
+            try {
+                const conversation = await prisma.conversation.create({
+                    data: {
+                        conversationParticipant: {
+                            createMany: {
+                                data: participantId.map((id) => ({
+                                    userId: id,
+                                    hasSeenLatestMessage: id === userId
+                                }))
+                            }
+                        }
+                    },
+                    include: conversationPopulated
+                });
+            } catch (error) {
+                console.log(error, "create conversation error");
+                throw new ApolloError("Error creating conversation");
+            }
         }
     }
 };
 
+export const participantPopulated =
+    Prisma.validator<Prisma.ConversationParticipantInclude>()({
+        user: {
+            select: {
+                id: true,
+                username: true
+            }
+        }
+    });
+
+export const conversationPopulated =
+    Prisma.validator<Prisma.ConversationInclude>()({
+        conversationParticipant: {
+            include: participantPopulated
+        },
+        latestMessage: {
+            include: {
+                sender: {
+                    select: {
+                        id: true,
+                        username: true
+                    }
+                }
+            }
+        }
+    });
 export default resolvers;
