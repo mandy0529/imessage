73023d99a352f000d32c968c8032362489b0f1d5
minji <321963741@naver.com>
2023-03-14 08:47:04 +0900

    added comment

--- backend/src/graphql/resolvers/conversation.ts
+++ backend/src/graphql/resolvers/conversation.ts
@@ -17,14 +17,18 @@ const resolvers = {
                 throw new ApolloError("Not authorized");
             }
 
+            //  session에서 나의 userId 꺼내기
             const {
                 user: { id: userId }
             } = session;
 
+            //  prisma conversation model에 create
             try {
                 const conversation = await prisma.conversation.create({
                     data: {
+                        // createConversation해줄때, conversation prisma model안에 conversationParticipant에도 같이 추가해줄 것
                         conversationParticipant: {
+                            //  참가하는 id가 여러개이므로, createMany로 다 만들어줄것이고,
                             createMany: {
                                 data: participantId.map((id) => ({
                                     userId: id,
@@ -33,6 +37,7 @@ const resolvers = {
                             }
                         }
                     },
+                    //  validator로 로직 아래로 뺌
                     include: conversationPopulated
                 });
 
@@ -49,6 +54,7 @@ const resolvers = {
 
 export const participantPopulated =
     Prisma.validator<Prisma.ConversationParticipantInclude>()({
+        //  참가하는 user의 정보 frontend를 위해 추가해주기
         user: {
             select: {
                 id: true,
@@ -62,6 +68,8 @@ export const conversationPopulated =
         conversationParticipant: {
             include: participantPopulated
         },
+        //  latestMessage를 Message model에서 relation 해온것이므로 마지막 메세지가 무엇인지 누가보낸것인지 정보를 같이 들려줘야하기때문에 sender를 보내주자
+        //  sender도 마찬가지로 user 정보가 들어있어야한다.
         latestMessage: {
             include: {
                 sender: {
